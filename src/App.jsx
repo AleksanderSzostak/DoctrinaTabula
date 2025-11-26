1d92936aabb38656e2316f360a831063a6a61ea6
Xp_107 <126953420+Xp1070@users.noreply.github.com>
2025-11-26 10:15:35 +0100

    css 1
    Dodałem początek layoutu strony.

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,35 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="header" class="h-2/25 w-full flex flex-row flex-auto">
+      <div id="menu" class="bg-blue-500 h-full basis-1/2 flex flex-auto">
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="userMenu" class="bg-green-500 h-full basis-1/10 flex">
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+    
+    <div id="flexBlock" class="flex flex-row h-23/25 w-full">
+      <div id="sidebar" class="h-full w-1/4 flex flex-col flex-auto">
+          <div id="titleBar" class="bg-yellow-500 basis-1/10 w-full flex">
+
+          </div>
+          <div id="explorer" class="bg-pink-500 basis-9/10 w-full flex flex-auto">
+
+          </div>
+      </div>
+      <div id="main" class="h-full w-3/4 flex flex-col flex-auto">
+        <div id="fish" class="bg-purple-500 h-full basis-3/4 flex flex-auto flex-wrap justify-center content-center">
+          <div id="flashcard" class="bg-blue-300 h-3/5 w-3/5 flex flex-wrap rounded-4xl justify-center content-center hover:rotate-x-180">Tekst</div>
+        </div>
+        <div id="footer" class="bg-black basis-2/25 w-full flex flex-row">
+
+        </div>
+      </div>
+    </div>
+    
+    
     </>
   )
 }
