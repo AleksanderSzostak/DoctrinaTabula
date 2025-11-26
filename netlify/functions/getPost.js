830600b74536c809f213120cb1e0d2f56e06d277
Franciszek Blachnio <Franciszek.Blacnio@tm1.edu.pl>
2025-11-26 10:19:20 +0100

    ee

--- netlify/functions/getPost.js
+++ netlify/functions/getPost.js
@@ -4,7 +4,7 @@ export const handler = async (event, context) => {
   const sql = neon(); // NETLIFY_DATABASE_URL is used automatically
   const id = event.queryStringParameters.id || 1;
 
-  const [post] = await sql`SELECT * FROM posts WHERE id = ${id}`;
+  const [post] = await sql`SELECT * FROM fiszki WHERE id = ${id}`;
 
   return {
     statusCode: 200,
